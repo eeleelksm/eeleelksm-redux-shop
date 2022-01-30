db3ff62aa36a84fa5451b05a475737d9921b25f4
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-30 17:38:00 -0500

    build

---
+++ client/build/precache-manifest.86e86667825c029f2e80e62291ea60a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6b9e7810ba8fd76902af7006b34936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a5067ad8cf51d97ae9",
+    "url": "/static/css/main.dc614c6b.chunk.css"
+  },
+  {
+    "revision": "ceec613c815ea5432075",
+    "url": "/static/js/2.9b7f7805.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9b7f7805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a5067ad8cf51d97ae9",
+    "url": "/static/js/main.37e2ae23.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
